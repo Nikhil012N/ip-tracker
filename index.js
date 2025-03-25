2d37f5fac7099cf8c03cd7d3c8eebc822ee4cf76
Nikhil <133760076+Nikhil012N@users.noreply.github.com>
2025-03-26 01:33:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const useragent = require('useragent');
+const geoip = require('geoip-lite');
+const requestIp = require('request-ip');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+// Middleware to handle JSON requests
+app.use(express.json());
+
+app.get('/user-details', async (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  const agent = useragent.parse(req.headers['user-agent']);
+  const geo = geoip.lookup(clientIp);
+  const referrer = req.headers.referer || req.headers.referrer;
+  const headers = req.headers;
+  const queryParams = req.query;
+  const userDetails = {
+    ipAddress: clientIp,
+    userAgent: agent,
+    geoLocation: geo,
+    referrer,
+    headers,
+    queryParams,
+  };
+
+  res.json(userDetails);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
